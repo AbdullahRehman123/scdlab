eb363025c38b8a98c852d00267ea0f5945d01f41
AbdullahRehman123 <abdullahrahmandp@gmail.com>
2021-02-22 21:50:01 +0500

    SecondVersAssign1

--- ScdAssign1/src/scdassign1/Airplane.java
+++ ScdAssign1/src/scdassign1/Airplane.java
@@ -3,6 +3,7 @@ package scdassign1;
 public class Airplane implements Flying{
     
     @Override // interface Flying
+    //comment added after pushing 1
     public void fly(){
         System.out.println("The Airplane is flying");
     }
